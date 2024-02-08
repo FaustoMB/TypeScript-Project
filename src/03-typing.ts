eb3a8e1bd73351a83d6aaf7984a983000ad4bf73
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-02-08 14:40:17 -0500

    Tipos Inferidos

--- src/03-typing.ts
+++ src/03-typing.ts
@@ -1,2 +1,13 @@
-let myProductName = 'Product 1';
-let myProductPrice = 123;
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myproductStock = 1000;
+  const myProductName2 = 'Product 1';
+})();
+
