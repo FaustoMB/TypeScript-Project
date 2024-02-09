14582ae5f08670faf278ad9fde451cf29065b318
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-02-08 21:34:02 -0500

    funciones no soportadas

---
+++ src/17-load.ts
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+
+const data = [
+  {
+    username: 'fausto',
+    role: 'admin'
+  },
+  {
+    username: 'valentina',
+    role: 'seller'
+  },
+  {
+    username: 'zulema',
+    role: 'seller'
+  },
+  {
+    username: 'santiago',
+    role: 'customer'
+  }
+];
+
+const rta = _.groupBy(data, (item) => item.role);
+console.log(rta)
+
+
